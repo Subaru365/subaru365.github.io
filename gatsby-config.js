40a156213e784d0646a5610fa2fc1747a0f9ceeb
Akira Murata <akira-murata@dac.co.jp>
2020-06-08 01:28:11 +0900

    fix: adapt plugin configurations of manifest and gtag
    * add a favicon
    * track clicks on outbound links

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
         theme_color: '#6b37bf',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone'
-        // icon: 'public/favicon.png' // This path is relative to the root of the site.
+        display: 'standalone',
+        icon: 'static/profile.png'
       }
     },
     {
@@ -52,10 +52,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: ["G-NPJ79BKVMN"],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
       },
     },
   ]
